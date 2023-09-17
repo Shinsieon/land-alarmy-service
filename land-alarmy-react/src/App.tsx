ee667886c98b6d0f258d7edda4d61ed8d4ae2807
Shinsieon <coolguysiun@gmail.com>
2023-09-17 19:42:11 +0900

    front design

--- land-alarmy-react/src/App.tsx
+++ land-alarmy-react/src/App.tsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import {
   Button,
   FormControl,
@@ -10,6 +9,8 @@ import {
   Checkbox,
   Select,
   MenuItem,
+  Typography,
+  Box,
 } from "@mui/material";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -19,7 +20,6 @@ export type TLocalCodes = {
   code: string;
 };
 function App() {
-  console.log("hello");
   const [data, setData] = useState<TLocalCodes[]>([]);
   const sizeChange = () => {};
   useEffect(() => {
@@ -52,18 +52,40 @@ function App() {
     fetchData().then((res) => setData(res));
   }, []);
   return (
-    <div className="App">
-      <FormControl>
-        <FormLabel>원하는 지역을 입력해주세요</FormLabel>
+    <Box sx={{ display: "flex", justifyContent: "center" }}>
+      <FormControl
+        sx={{
+          margin: "50px 0",
+          textAlign: "left",
+          display: "flex",
+          flexDirection: "column",
+          gap: "10px",
+          width: "500px",
+        }}
+      >
+        <Typography variant="h4">매물 알리미 서비스 등록</Typography>
+
+        <FormLabel>
+          <b>원하는 지역을 입력해주세요</b>
+        </FormLabel>
         <Autocomplete
           disablePortal
           id="combo-box-demo"
           options={data}
-          sx={{ width: 300 }}
+          size="small"
+          sx={{ width: "100%" }}
           renderInput={(params) => <TextField {...params} label="시/군/구" />}
         />
-        <FormLabel>거래방식</FormLabel>
-        <FormGroup sx={{ display: "flex", flexDirection: "row" }}>
+        <FormLabel>
+          <b>거래방식</b>
+        </FormLabel>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
           <FormControlLabel
             control={<Checkbox defaultChecked />}
             label="전체"
@@ -71,28 +93,42 @@ function App() {
           <FormControlLabel control={<Checkbox />} label="매매" />
           <FormControlLabel control={<Checkbox />} label="전세" />
           <FormControlLabel control={<Checkbox />} label="월세" />
-        </FormGroup>
-        <FormLabel>가격대</FormLabel>
-        <FormGroup sx={{ display: "flex", flexDirection: "row" }}>
+        </Box>
+        <FormLabel>
+          <b>가격대</b>
+        </FormLabel>
+        <FormGroup
+          sx={{
+            display: "flex",
+            flexDirection: "row",
+            width: "100%",
+            justifyContent: "space-between",
+          }}
+        >
           <TextField
             size="small"
             id="outlined-basic"
             label="최저가"
             variant="outlined"
           />
-          ~
+          <Typography> ~ </Typography>
           <TextField
             size="small"
             id="outlined-basic"
-            label="최저가"
+            label="최대가"
             variant="outlined"
           />
         </FormGroup>
+        <FormLabel>
+          <b>평수</b>
+        </FormLabel>
+
         <FormGroup sx={{ display: "flex", flexDirection: "row" }}>
           <Select
             labelId="demo-simple-select-label"
             id="demo-simple-select"
             value={0}
+            fullWidth={true}
             label="Age"
             onChange={sizeChange}
           >
@@ -106,9 +142,21 @@ function App() {
             <MenuItem value={7}>70평~</MenuItem>
           </Select>
         </FormGroup>
-        <Button>Submit</Button>
+        <FormLabel>
+          <b>휴대전화 번호</b>
+        </FormLabel>
+        <TextField
+          size="small"
+          id="outlined-basic"
+          label="Phone number"
+          variant="outlined"
+          type="tel"
+        />
+        <Button variant="contained" size="large" sx={{ margin: "25px 0" }}>
+          Submit
+        </Button>
       </FormControl>
-    </div>
+    </Box>
   );
 }
 
